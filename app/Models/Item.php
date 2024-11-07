67a758a410db09845886741afc05faba71c979a1
Moses Alcantara <infinitech.mosesalcantara@gmail.com>
2024-11-07 10:32:15 +0800

    items migration and model

---
+++ app/Models/Item.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+        'category',
+        'width',
+        'height',
+        'image',
+    ];
+
+    public static function booted() {
+        static::creating(function (Item $record) {
+            $record->id = Str::ulid();
+        });
+    }
+}
