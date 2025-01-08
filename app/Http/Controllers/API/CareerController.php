8372be703e32f8caa812e14ab5b27817d8949406
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-01-08 10:15:30 +0800

    careers

--- app/Http/Controllers/API/CareerController.php
+++ app/Http/Controllers/API/CareerController.php
@@ -4,65 +4,100 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
 use App\Traits\Uploadable;
+
 use App\Models\Career as Model;
 
 class CareerController extends Controller
 {
     use Uploadable;
+    
+    public $model = "Career";
 
     public function getAll()
     {
         $records = Model::all();
-        $data = ['code' => 200, 'records' => $records];
-        return response($data);
+        $code = 200;
+        $response = ['message' => "Fetched $this->model" . "s", 'records' => $records];
+        return response()->json($response, $code);
     }
 
     public function get($id)
     {
-        $record = Model::findOrFail($id);
-        $data = ['code' => 200, 'record' => $record];
-        return response($data);
+        $record = Model::find($id);
+        if ($record) {
+            $code = 200;
+            $response = ['message' => "Fetched $this->model", 'record' => $record];
+        }
+        else {
+            $code = 404;
+            $response = ['message' => "$this->model Not Found"];
+        }
+        return response()->json($response, $code);
     }
 
-    public function add(Request $request)
+    public function create(Request $request)
     {
         $validated = $request->validate([
+            'position' => 'required',
             'referrer' => 'required',
             'sub_agent' => 'required',
             'broker' => 'required',
             'partner' => 'required',
-            'position' => 'required',
-            'image' => 'required|image|max:2048',
+            'image' => 'required',
         ]);
 
         $key = 'image';
-        $validated[$key] = $this->upload($validated[$key], 'uploads/careers/images');
+        if ($request->hasFile($key)) {
+            $validated[$key] = $this->upload($request->file($key), "careers/images");
+        }
 
-        Model::create($validated);
-        $data = ['code' => 200];
-
-        return response($data);
+        $record = Model::create($validated);
+        $code = 201;
+        $response = ['message' => "Created $this->model", 'record' => $record];
+        return response()->json($response, $code);
     }
 
-    // public function update(Request $request, $id)
-    // {
-    //     $request->validate([
-    //         'name' => 'required',
-    //         'description' => 'required',
-    //     ]);
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'id' => 'required|exists:careers,id',
+            'position' => 'required',
+            'referrer' => 'required',
+            'sub_agent' => 'required',
+            'broker' => 'required',
+            'partner' => 'required',
+            'image' => 'nullable',
+        ]);
 
-    //     $record = Model::find($id);
-    //     $record->update($request->all());
+        $record = Model::find($validated['id']);
 
-    //     return response(['code' => 200]);
-    // }
+        $key = 'image';
+        if ($request->hasFile($key)) {
+            Storage::disk('s3')->delete("careers/images/$record[$key]");
+            $validated[$key] = $this->upload($request->file($key), "careers/images");
+        }
+
+        $record->update($validated);
+        $code = 200;
+        $response = ['message' => "Updated $this->model", 'record' => $record];
+        return response()->json($response, $code);
+    }
 
     public function delete($id)
     {
-        $record = Model::findOrFail($id);
-        $record->delete();
-        return response(['code' => 200]);
+        $record = Model::find($id);
+        if ($record) {
+            Storage::disk('s3')->delete("careers/images/$record->image");
+            $record->delete();
+            $code = 200;
+            $response = ['message' => "Deleted $this->model"];
+        }
+        else {
+            $code = 404;
+            $response = ['message' => "$this->model Not Found"];
+        }
+        return response($response, $code);
     }
 }
