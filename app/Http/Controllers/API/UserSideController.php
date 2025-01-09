bba7cc859043cab3f055c647bc733f405db56ed4
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-01-09 09:44:01 +0800

    user side - check user id

--- app/Http/Controllers/API/UserSideController.php
+++ app/Http/Controllers/API/UserSideController.php
@@ -18,27 +18,23 @@ class UserSideController extends Controller
 
     public function getUser(Request $request)
     {
-        // if ($user_id = $request->bearerToken()) {
-        //     $relations = ['profile', 'certificates', 'images', 'testimonials', 'properties', 'appointments', 'listings'];
-        //     $record = User::with($relations)->where('id', $user_id)->first();
-        //     if ($record) {
-        //         $code = 200;
-        //         $response = ['message' => "Fetched User", 'record' => $record];
-        //     } else {
-        //         $code = 404;
-        //         $response = ['message' => "User Not Found"];
-        //     }
-        // } else {
-        //     $code = 401;
-        //     $response = ['message' => "User Not Authenticated"];
-        // }
-        // return response()->json($response, $code);
-        return response()->json($request->bearerToken());
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
+            $relations = ['profile', 'certificates', 'images', 'testimonials', 'properties', 'appointments', 'listings'];
+            $record = User::with($relations)->where('id', $user_id)->first();
+            $code = 200;
+            $response = ['message' => "Fetched User", 'record' => $record];
+        } else {
+            $code = 401;
+            $response = ['message' => "User Not Authenticated"];
+        }
+        return response()->json($response, $code);
     }
 
     public function propertiesGetAll(Request $request)
     {
-        if ($user_id = $request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $relations = ['user', 'plan', 'buildings', 'facilities', 'features', 'units'];
             $records = Property::with($relations)->where('user_id', $user_id)->get();
             $code = 200;
@@ -52,7 +48,8 @@ class UserSideController extends Controller
 
     public function propertiesGet(Request $request)
     {
-        if ($user_id = $request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $relations = ['user', 'plan', 'buildings', 'facilities', 'features', 'units'];
             $where = [['id', $request->id], ['user_id', $user_id]];
 
@@ -73,7 +70,8 @@ class UserSideController extends Controller
 
     public function articlesGetAll(Request $request)
     {
-        if ($request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $records = Article::all();
             $code = 200;
             $response = ['message' => "Fetched Articles", 'records' => $records];
@@ -86,7 +84,8 @@ class UserSideController extends Controller
 
     public function articlesGet(Request $request)
     {
-        if ($request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $record = Article::find($request->id);
             if ($record) {
                 $code = 200;
@@ -104,7 +103,8 @@ class UserSideController extends Controller
 
     public function careersGetAll(Request $request)
     {
-        if ($request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $records = Career::with('applications')->get();
             $code = 200;
             $response = ['message' => "Fetched Careers", 'records' => $records];
@@ -117,7 +117,8 @@ class UserSideController extends Controller
 
     public function careersGet(Request $request)
     {
-        if ($request->bearerToken()) {
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
             $record = Career::with('applications')->where('id', $request->id)->first();
             if ($record) {
                 $code = 200;
@@ -135,34 +136,40 @@ class UserSideController extends Controller
 
     public function submitProperty(Request $request)
     {
-        $validated = $request->validate([
-            'user_id' => 'required|exists:users,id',
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required',
-            'unit_name' => 'required',
-            'unit_type' => 'required',
-            'unit_location' => 'required',
-            'unit_price' => 'required|decimal:0,2',
-            'status' => 'required',
-            'images' => 'required',
-        ]);
+        $user_id = $request->header('user_id');
+        if (User::find($user_id)) {
+            $validated = $request->validate([
+                'user_id' => 'required|exists:users,id',
+                'name' => 'required',
+                'email' => 'required|email',
+                'phone' => 'required',
+                'unit_name' => 'required',
+                'unit_type' => 'required',
+                'unit_location' => 'required',
+                'unit_price' => 'required|decimal:0,2',
+                'status' => 'required',
+                'images' => 'required',
+            ]);
 
-        $key = 'images';
-        if ($request[$key]) {
-            $images = [];
-            foreach ($request[$key] as $image) {
-                array_push($images, $this->upload($image, "listings"));
+            $key = 'images';
+            if ($request[$key]) {
+                $images = [];
+                foreach ($request[$key] as $image) {
+                    array_push($images, $this->upload($image, "listings"));
+                }
+                $validated[$key] = json_encode($images);
             }
-            $validated[$key] = json_encode($images);
-        }
 
-        $record = Listing::create($validated);
-        $code = 201;
-        $response = [
-            'message' => "Submitted Property Details",
-            'record' => $record,
-        ];
+            $record = Listing::create($validated);
+            $code = 201;
+            $response = [
+                'message' => "Submitted Property Details",
+                'record' => $record,
+            ];
+        } else {
+            $code = 401;
+            $response = ['message' => "User Not Authenticated"];
+        }
         return response()->json($response, $code);
     }
 }
