c8403a4d2f75b585254203f419251e289cf6de93
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-01-13 16:19:44 +0800

    add contracts to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,11 @@ class User extends Authenticatable
         return $this->hasMany(Testimonial::class);
     }
 
+    public function contracts(): HasMany
+    {
+        return $this->hasMany(Contract::class);
+    }
+
     public function videos(): HasMany
     {
         return $this->hasMany(Video::class);
