511ff9770e0c67000b42c332a7ac9a6ce55dadf7
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2025-02-07 11:04:50 +0800

    asdf

--- app/Http/Controllers/API/Admin/PropertyController.php
+++ app/Http/Controllers/API/Admin/PropertyController.php
@@ -21,7 +21,7 @@ class PropertyController extends Controller
         $user =  PersonalAccessToken::findToken($request->bearerToken())->tokenable;
         $relations = ['user', 'plan', 'buildings', 'facilities', 'features', 'units'];
         if ($user->type == "Admin") {
-            $records = Model::with($relations)->get();
+            $records = Model::with($relations)->orderBy('created_at', 'desc')->get();
         } else if ($user->type == "Agent") {
             $records = Model::with($relations)->where('user_id', $user->id)->orderBy('status')->get();
         }
