278aa2f4bca03f8a961c6e962261f031c857809a
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-26 15:22:28 +0800

    test

--- app/Http/Controllers/API/UserSideController.php
+++ app/Http/Controllers/API/UserSideController.php
@@ -168,6 +168,11 @@ class UserSideController extends Controller
             array_push($where, ['location', 'LIKE', "%$location%"]);
         }
 
+        $unit_type = $request->query('unit_type');
+        if ($unit_type) {
+            array_push($where, ['max_price', $unit_type]);
+        }
+
         $min_price = $request->query('min_price');
         if ($min_price) {
             array_push($where, ['min_price', '>=', $min_price]);
@@ -176,11 +181,6 @@ class UserSideController extends Controller
         $max_price = $request->query('max_price');
         if ($max_price) {
             array_push($where, ['min_price', '<=', $max_price]);
-        }
-
-        $unit_type = $request->query('unit_type');
-        if ($unit_type) {
-            array_push($where, ['max_price', $unit_type]);
         }
 
         $relations = ['user', 'plan', 'buildings', 'facilities', 'features', 'units'];
