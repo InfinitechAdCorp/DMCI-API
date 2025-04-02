c3a89fc712cb3f5f15ae194bc33d710fee6910b5
unknown <giolo.evora@gmail.com>
2025-04-02 11:49:40 +0800

    update

--- app/Http/Controllers/API/UserSideController.php
+++ app/Http/Controllers/API/UserSideController.php
@@ -50,6 +50,17 @@ class UserSideController extends Controller
     public function propertiesGetAll(Request $request)
     {
         $user_id = $request->header('user-id');
+        $relations = ['user'];
+        $records = Property::with($relations)->where('user_id', $user_id)->orderBy('status')->get();
+        $code = 200;
+        $response = ['message' => "Fetched Properties", 'records' => $records];
+        return response()->json($response, $code);
+    }
+
+    // new get all properties
+    public function propertyGetAll(Request $request)
+    {
+        $user_id = $request->header('user-id');
         $relations = ['user', 'plan', 'buildings', 'facilities', 'features', 'units'];
         $records = Property::with($relations)->where('user_id', $user_id)->orderBy('status')->get();
         $code = 200;
--- routes/api.php
+++ routes/api.php
@@ -264,6 +264,12 @@ Route::middleware('throttle:150,1')->group(function () {
             Route::get('{id}', [UserSideController::class, 'propertiesGet']);
         });
 
+        Route::prefix('property')->group(function () {
+            Route::get('', [UserSideController::class, 'propertyGetAll']);
+            Route::get('{id}', [UserSideController::class, 'propertiesGet']);
+        });
+
+
         Route::prefix('listings')->group(function () {
             Route::get('', [UserSideController::class, 'listingsGetAll']);
             Route::get('{id}', [UserSideController::class, 'listingsGet']);
