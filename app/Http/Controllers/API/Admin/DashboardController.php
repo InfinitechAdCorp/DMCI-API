868854bd26265d5f27542a4fdb94ee3aff79e42e
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-04-04 15:22:33 +0800

    update

--- app/Http/Controllers/API/Admin/DashboardController.php
+++ app/Http/Controllers/API/Admin/DashboardController.php
@@ -7,17 +7,10 @@ use Illuminate\Http\Request;
 use Laravel\Sanctum\PersonalAccessToken;
 
 use App\Models\Property;
-use App\Models\Listing;
 use App\Models\Appointment;
 use App\Models\Inquiry;
 use App\Models\Application;
-use App\Models\Plan;
-use App\Models\Question;
-use App\Models\Article;
-use App\Models\Testimonial;
-use App\Models\Video;
-use App\Models\Contract;
-use App\Models\Career;
+
 
 class DashboardController extends Controller
 {
@@ -26,37 +19,15 @@ class DashboardController extends Controller
         $user =  PersonalAccessToken::findToken($request->bearerToken())->tokenable;
 
         $properties = Property::get()->count();
-        $rfoProperties = Property::where('status', 'Ready For Occupancy')->get()->count();
-        $underConstructionProperties = Property::where('status', 'Under Construction')->get()->count();
-        $newProperties = Property::where('status', 'New')->get()->count();
-        $listings = Listing::get()->count();
         $inquiries = Inquiry::get()->count();
         $viewings = Appointment::get()->count();
         $applications = Application::get()->count();
-        $plans = Plan::get()->count();
-        $questions = Question::get()->count();
-        $articles = Article::get()->count();
-        $testimonials = Testimonial::get()->count();
-        $videos = Video::get()->count();
-        $contracts = Contract::get()->count();
-        $careers = Career::get()->count();
 
         $records = [
             'properties' => $properties,
-            'rfoProperties' => $rfoProperties,
-            'underConstructionProperties' => $underConstructionProperties,
-            'newProperties' => $newProperties,
-            'listings' => $listings,
             'inquiries' => $inquiries,
             'viewings' => $viewings,
             'applications' => $applications,
-            'plans' => $plans,
-            'questions' => $questions,
-            'articles' => $articles,
-            'testimonials' => $testimonials,
-            'videos' => $videos,
-            'contracts' => $contracts,
-            'careers' => $careers,
         ];
         $code = 200;
         $response = ['message' => "Fetched Counts", 'records' => $records];
